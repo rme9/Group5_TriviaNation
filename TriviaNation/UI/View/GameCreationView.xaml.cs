f4de63650113f03fa594f53b9d6c87626b52b14c
Rebecca Elliff <rme9@students.uwf.edu>
2018-03-01 10:31:51 -0600

    created ui folder

---
+++ TriviaNation/UI/View/GameCreationView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TriviaNation.UI.View
+{
+    /// <summary>
+    /// Interaction logic for GameCreationView.xaml
+    /// </summary>
+    public partial class GameCreationView : Window
+    {
+        public GameCreationView()
+        {
+            InitializeComponent();
+        }
+    }
+}
