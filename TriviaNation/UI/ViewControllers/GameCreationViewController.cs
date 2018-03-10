1a432500eae21d3823bc175cc73a6c24a0f1a799
Rebecca Elliff <rme9@students.uwf.edu>
2018-03-09 18:01:41 -0600

    Renamed Controller -> ViewControllers

---
+++ TriviaNation/UI/ViewControllers/GameCreationViewController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaNation.UI.Controller
+{
+    class GameCreationViewController
+    {
+        protected void OnStudentListChanged(string name)
+        {
+            PropertyChangedEventHandler handler = StudentListChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public event PropertyChangedEventHandler StudentListChanged;
+    }
+}
