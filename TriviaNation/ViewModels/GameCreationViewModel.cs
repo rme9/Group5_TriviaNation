be044b988368df177ea33d5451a91717fcf06fd2
Rebecca Elliff <rme9@students.uwf.edu>
2018-03-13 22:09:11 -0500

    TES-1 Cleaned up GameCreationViewController

--- TriviaNation/ViewModels/GameCreationViewModel.cs
+++ TriviaNation/ViewModels/GameCreationViewModel.cs
@@ -13,26 +13,6 @@ namespace TriviaNation.ViewModels
 	// Defines the data context of a GameCreationView
 	class GameCreationViewModel : IViewModel
 	{
-		public GameCreationViewModel()
-		{
-
-		}
-
-		#region CancelCommand
-
-		public void ExecuteCancelCommand(object sender)
-		{
-			var button = sender as Button;
-			if (button == null)
-				return;
-
-			var win = Window.GetWindow(button);
-			if (win == null)
-				return;
-
-			win.Close();
-		}
-
-		#endregion
+		
 	}
 }
