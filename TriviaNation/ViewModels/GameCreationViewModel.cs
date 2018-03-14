300b2f606a5d4563b88dfa57c79fa6f144be7b19
Rebecca Elliff <rme9@students.uwf.edu>
2018-03-14 00:00:33 -0500

    Added data bindings to GameCreationView, updated viewmodel

--- TriviaNation/ViewModels/GameCreationViewModel.cs
+++ TriviaNation/ViewModels/GameCreationViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,12 +8,25 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using DevExpress.Mvvm;
+using TriviaNation.Models;
 
 namespace TriviaNation.ViewModels
 {
 	// Defines the data context of a GameCreationView
-	class GameCreationViewModel : IViewModel
+	public class GameCreationViewModel : IViewModel
 	{
-		
+		public string Name { get; set; }
+
+		public IQuestionBank SelectedQuestionBank { get; set; }
+
+		public ObservableCollection<IQuestionBank> AvailableQuestionBanks { get; set; }
+
+		public ObservableCollection<IUser> SelectedStudents { get; set; }
+
+		public ObservableCollection<IUser> AvailableStudents { get; set; }
+
+		public GameCreationViewModel()
+		{
+		}
 	}
 }
