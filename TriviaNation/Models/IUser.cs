3f83c4d1a9be42b970b06ae039aeb7dfe75168cc
Rebecca Elliff <rme9@students.uwf.edu>
2018-03-13 23:56:24 -0500

    TEA-1 Created generic User class and interface

---
+++ TriviaNation/Models/IUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaNation.Models
+{
+	public interface IUser
+	{
+		string Email { get; set; }
+
+		string Name { get; set; }
+	}
+}
---
+++ TriviaNation/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaNation.Models
+{
+	class User : IUser
+	{
+		public string Email { get; set; }
+
+		public string Name { get; set; }
+
+		public User(string Name, string Email)
+		{
+
+		}
+	}
+}
