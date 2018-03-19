bdd6bb2e5fdb596da094ab2a0cac643ea872b1b1
Rebecca Elliff <rme9@students.uwf.edu>
2018-03-18 21:03:25 -0500

    Rebased onto Develop, Moved some of the files up in the directory, removed build output files

--- TriviaNation/MainWindow.xaml.cs
+++ TriviaNation/MainWindow.xaml.cs
@@ -15,14 +15,31 @@ using System.Windows.Shapes;
 
 namespace TriviaNation
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
-	}
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ImageBrush myBrush = new ImageBrush();
+            myBrush.ImageSource = new BitmapImage(new Uri("D:/Programming/SE2/TriviaNation/TriviaNation/TriviaNation/Resources/background.gif", UriKind.Absolute));
+            this.Background = myBrush;
+        }
+
+        private void showInformation(object sender, RoutedEventArgs e)
+        {
+            if (((RadioButton)sender).IsChecked == true)
+            {
+                InformationPanel.Visibility = Visibility.Visible;
+                Information_Info.Visibility = Visibility.Visible;
+                Information_Controlled.Visibility = Visibility.Visible;
+                AttackButton.Visibility = Visibility.Visible;
+                DefendButton.Visibility = Visibility.Visible;
+
+                Information_Info.Content = ((RadioButton)sender).Name;
+            }
+        }
+    }
 }
