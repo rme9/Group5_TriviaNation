02ffe75f8f00dd50f053bfe1814455f2fcd33769
Joree Hubbell <jlh126@students.uwf.edu>
2018-03-23 01:38:48 -0500

    Inital Binding test(make sure that the binding work is correct)

--- TriviaNation/ViewModels/AddQuestionBankViewModel.cs
+++ TriviaNation/ViewModels/AddQuestionBankViewModel.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TriviaNation.Models;
 
 namespace TriviaNation.ViewModels
 {
-    public class AddQuestionBankViewModel
+    public class AddQuestionBankViewModel: IQuestion, IViewModel
     {
+        public string Body { get; set; }
+        public string CorrectAnswer { get; set; }
+        public List<string> AlternateAnswers { get; set; }
     }
 }
