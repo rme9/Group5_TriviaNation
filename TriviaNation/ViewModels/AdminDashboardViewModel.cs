d6fc2878ba291669a92ef509d03561c6e4c6d550
Rebecca Elliff <rme9@students.uwf.edu>
2018-03-24 16:12:00 -0500

    Admin Dashboard view properly loads data from the database

--- TriviaNation/ViewModels/AdminDashboardViewModel.cs
+++ TriviaNation/ViewModels/AdminDashboardViewModel.cs
@@ -3,12 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using TriviaNation.Drivers;
+using TriviaNation.Models;
+using TriviaNation.Util;
 
 namespace TriviaNation.ViewModels
 {
 	public class AdminDashboardViewModel
 	{
+		public List<StudentUser> AllStudents { get; set; }
 
+		public List<IQuestionBank> AllQuestionBanks { get; set; }
+
+		public AdminDashboardViewModel()
+		{
+			using (var db = new DynamoDBDriver())
+			{
+				var currentUser = Application.Current.Properties["LoggedInUserId"] as string;
+				AllStudents = db.GetAllUsersByInstructor(currentUser);
+				AllQuestionBanks = db.GetQuestionBanksByInstructor(currentUser);
+			}
+		}
 
 		#region ManageGameSessions
 
