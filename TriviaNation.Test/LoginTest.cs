4885f92b59a17681c2e8a8489dc3623a42cbb29d
Rebecca Elliff <rme9@students.uwf.edu>
2018-04-15 23:13:22 -0500

    Added test for new login functionality

---
+++ TriviaNation.Test/LoginTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriviaNation.Core.Drivers;
+using TriviaNation.Core.Models;
+
+namespace TriviaNation.Test
+{
+	[TestClass]
+	public class LoginTest
+	{
+		private string _UserName = "rme9@students.uwf.edu";
+		private string _Password = "password";
+		private string _UserType = "admin";
+
+		[TestMethod]
+		public void Login()
+		{
+			using (var driver = new LoginDriver())
+			{
+				var user = driver.Login(_UserName, _Password, _UserType).Result;
+
+				Assert.IsNotNull(user);
+				Assert.IsTrue((user as AdminUser) != null);
+				Assert.AreEqual(_UserName, user.Email);
+			}
+		}
+	}
+}
