efc0cc8eb048ebe6faab3ce33b1a9ee1e1b39868
Zach Porter <zach.scot.porter@gmail.com>
2018-04-17 21:11:52 -0500

    added territory model

---
+++ TriviaNation.Student/Models/Territories.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaNation.Student.Models
+{
+    class Territories
+    {
+        private string Name;
+        private string ControllerName = null;
+        private bool ControlStatus = false;
+
+        public Territories(string name, bool isConstrolled, string controllerName)
+        {
+            Name = name;
+            ControlStatus = isConstrolled;
+            ControllerName = controllerName;
+
+        }
+    }
+}
