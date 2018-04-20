27dfd2373371539640e7acb8c0e3e94d87e259e8
Zach Porter <zach.scot.porter@gmail.com>
2018-04-19 21:25:12 -0500

    adding previous functionality to the game board

--- TriviaNation.Student/Views/GameBoardView.xaml.cs
+++ TriviaNation.Student/Views/GameBoardView.xaml.cs
@@ -20,14 +20,66 @@ namespace TriviaNation.Student.Views
     /// </summary>
     public partial class GameBoardView : UserControl
     {
+        List<Rectangle> territories = new List<Rectangle>();
+
         public GameBoardView()
         {
             InitializeComponent();
+            AddFunctionality();
+        }
+
+        private void AddFunctionality()
+        {
+            foreach (Rectangle x in boardGrid.Children.OfType<Rectangle>())
+            {
+                if (x.Name != "Information_Panel")
+                {
+                    territories.Add(x);
+                    x.MouseLeftButtonDown += Rectangle_MouseLeftButtonDown;
+                    x.Opacity = 0;
+                }
+            }
+
         }
 
         private void AttackButton_onClick(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void showInformation(object sender, RoutedEventArgs e)
+        {
+            Information_Panel.Visibility = Visibility.Visible;
+            Information_Info.Visibility = Visibility.Visible;
+            Information_Controlled.Visibility = Visibility.Visible;
+            Information_ControlName.Visibility = Visibility.Visible;
+            //AttackButton.Visibility = Visibility.Visible;
+            //DefendButton.Visibility = Visibility.Visible;
+
+            Information_Info.Content = ((Rectangle)sender).Name;
+            if (Information_ControlName.Content.Equals(""))
+            {
+                Information_ControlName.Content = "Uncontested.";
+                AttackButton.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void ResetTerritories()
+        {
+            foreach (Rectangle x in territories)
+            {
+                x.Opacity = 0;
+            }
+        }
+
+        private void Rectangle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ResetTerritories();
+            ((Rectangle)sender).Opacity = 1;
+            ((Rectangle)sender).Fill = new SolidColorBrush(Colors.Blue);
+            ((Rectangle)sender).Fill.Opacity = .3;
+            ((Rectangle)sender).StrokeThickness = 3;
+            showInformation(((Rectangle)sender), e);
         }
     }
 }
