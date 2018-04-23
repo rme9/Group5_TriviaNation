accdafb61948c4af07b80b79e4c62fbac260061d
Joree Hubbell <jlh126@students.uwf.edu>
2018-04-22 20:46:06 -0500

    added json via nuget and added drivers to finish functionality

---
+++ TriviaNation.Core/Drivers/IWebServiceDriver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TriviaNation.Core.Models;
+
+namespace TriviaNation.Core.Drivers
+{
+	public interface IWebServiceDriver
+	{
+		Task<bool> InsertUser(IUser newUser);
+
+		Task<bool> InsertQuestionBank(IQuestionBank newQuestionBank, string instructorEmail);
+
+		Task<bool >InsertGameSession(IGameSession newGameSession, string instructorsEmail);
+
+		Task<List<StudentUser>> GetAllUsersByInstructor(string instructorsEmail);
+
+		Task<List<IQuestionBank>> GetQuestionBanksByInstructor(string instructorsEmail);
+
+		Task<List<IGameSession>> GetGameSessionsByInstructor(string instructorsEmail);
+
+		Task<IUser> GetUserByEmail(string email);
+
+		Task<IQuestionBank> GetQuestionBankById(string uniqueId);
+
+		Task<IGameSession> GetGameSessionById(string uniqueId);
+	}
+}
